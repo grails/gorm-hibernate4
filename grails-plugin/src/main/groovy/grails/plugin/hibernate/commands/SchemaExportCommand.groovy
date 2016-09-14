70e4c972eb744d807b76cac292d5ae7631b8815f
Graeme Rocher <graeme.rocher@gmail.com>
2016-09-14 12:37:43 +0200

    Fix schema-export command for Hibernate 4

--- grails-plugin/src/main/groovy/grails/plugin/hibernate/commands/SchemaExportCommand.groovy
+++ grails-plugin/src/main/groovy/grails/plugin/hibernate/commands/SchemaExportCommand.groovy
@@ -20,7 +20,14 @@ import grails.dev.commands.ExecutionContext
 import grails.util.Environment
 import groovy.transform.CompileStatic
 import org.grails.build.parsing.CommandLine
+import org.grails.datastore.mapping.core.connections.ConnectionSource
+import org.grails.orm.hibernate.HibernateDatastore
 import org.grails.orm.hibernate.HibernateMappingContextSessionFactoryBean
+import org.hibernate.engine.spi.SessionFactoryImplementor
+import org.hibernate.metamodel.MetadataBuilder
+import org.hibernate.metamodel.MetadataSources
+import org.hibernate.metamodel.source.MetadataImplementor
+import org.hibernate.service.ServiceRegistry
 import org.hibernate.tool.hbm2ddl.SchemaExport as HibernateSchemaExport
 
 /**
@@ -52,15 +59,18 @@ class SchemaExportCommand implements ApplicationCommand {
         }
 
         def argsMap = commandLine.undeclaredOptions
-        String datasourceSuffix = argsMap.datasource ? '_' + argsMap.datasource : ''
+        String dataSourceName = argsMap.datasource ? argsMap.datasource : ConnectionSource.DEFAULT
 
         def file = new File(filename)
         file.parentFile.mkdirs()
 
-        def sessionFactory = applicationContext.getBean('&sessionFactory' + datasourceSuffix, HibernateMappingContextSessionFactoryBean)
-        def configuration = sessionFactory.configuration
+        HibernateDatastore hibernateDatastore = applicationContext.getBean("hibernateDatastore", HibernateDatastore)
+        hibernateDatastore = hibernateDatastore.getDatastoreForConnection(dataSourceName)
 
-        def schemaExport = new HibernateSchemaExport(configuration, sessionFactory.dataSource.connection)
+        def serviceRegistry = ((SessionFactoryImplementor)hibernateDatastore.sessionFactory).getServiceRegistry()
+                                                                                            .getParentServiceRegistry()
+        MetadataImplementor metadataImplementor = buildMetadata(serviceRegistry)
+        def schemaExport = new HibernateSchemaExport(metadataImplementor)
                 .setHaltOnError(true)
                 .setOutputFile(file.path)
                 .setDelimiter(';')
@@ -77,5 +87,11 @@ class SchemaExportCommand implements ApplicationCommand {
             return false
         }
         return true
+    }
+
+    protected MetadataImplementor buildMetadata(ServiceRegistry serviceRegistry) throws Exception {
+        final MetadataSources metadataSources = new MetadataSources( serviceRegistry );
+        final MetadataBuilder metadataBuilder = metadataSources.getMetadataBuilder();
+        return (MetadataImplementor) metadataBuilder.build();
     }
 }
