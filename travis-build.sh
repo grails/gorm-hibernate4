784cd7b00b6f44a2aeb591e66b68ea4024f946e2
graemerocher <graeme.rocher@gmail.com>
2017-03-27 11:38:27 +0200

    Stop gradle after tests complete

--- travis-build.sh
+++ travis-build.sh
@@ -14,9 +14,11 @@ if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
 else
 	./gradlew check --refresh-dependencies -no-daemon -x grails2-plugin:test -x gorm-hibernate4-spring-boot:test  || EXIT_STATUS=$?
 	if [[ $EXIT_STATUS -eq 0 ]]; then
+		./gradlew --stop
 	    ./gradlew grails2-plugin:test -no-daemon -Pgrails2.plugin.enabled=true || EXIT_STATUS=$?
 	fi
 	if [[ $EXIT_STATUS -eq 0 ]]; then 
+		./gradlew --stop
 	    ./gradlew gorm-hibernate4-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
 	fi
 
